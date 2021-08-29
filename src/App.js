8ddf27a44e49859c842c39999b7c07deb911bbe4
junya takaesu <junya.takaesu@gmail.com>
2021-08-29 13:32:34 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FetchData from "./Components/FetchData";
+
+function App() {
+  return (
+    <div className="container">
+      <FetchData />
+    </div>
+  );
+}
+
+export default App;
